e378a02fb88b82efec06c0bdfe9ccf046adfecb3
patw10 <62399846+patw10@users.noreply.github.com>
2020-03-24 14:59:03 +0100

    Revert "Revert "0.1""
    This reverts commit 3cd888266b6b224f94b3e8656e275aa0b00cbd47.

---
+++ NewGame/Assets/Scripts/Vechicle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vechicle : MonoBehaviour
+{
+    public Transform vechicle;
+    void Update()
+    {
+        transform.position = new Vector3(vechicle.position.x - 0.18f, vechicle.position.y + 0.737f, 0);
+        Vector3 chScale = transform.localScale;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            chScale.x = -1f;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            chScale.x = 1f;
+        }
+        transform.localScale = chScale;
+    }
+}
